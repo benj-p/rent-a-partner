ccfd65251853cfa1b1cbdfc047a5b238e37df93f
Ben Pham <benjamin.pham@gmail.com>
2019-02-26 13:55:22 +0000

    Completed basic new user form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :location, :age, :gender, :picture, :desired_gender, :desired_age, :bio, :is_partner])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :password, :email, :location, :age, :gender, :picture, :desired_gender, :desired_age, :bio, :is_partner])
+  end
+
+  def after_sign_up_path_for(resource)
+    root_path_for(resource)
   end
 end
