b3aedd7fffe31c82ae8d2548e44b925004596d35
Jonny Gates <jonny.gates@outlook.com>
2019-02-26 11:21:45 +0000

    Add new and create actions to personalities

--- app/controllers/personalities_controller.rb
+++ app/controllers/personalities_controller.rb
@@ -5,10 +5,17 @@ class PersonalitiesController < ApplicationController
   end
 
   def new
-
+    @personality = Personality.new
   end
 
   def create
+    @personality = Personality.new(personality_params)
+    @personality.user = current_user
+    if @personality.save
+      redirect_to personality_path(@personality)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,4 +25,15 @@ class PersonalitiesController < ApplicationController
   def update
 
   end
+
+  private
+
+  def set_personality
+    @personality = Personality.find(params[:id])
+  end
+
+  def personality_params
+    params.require(:personality).permit(:first_name, :last_name, :bio, :desired_gender, :desired_age, :picture, :price_per_day)
+  end
+
 end
