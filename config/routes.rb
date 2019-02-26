d269bc4fabd66a225a73aa00820e565042909fe4
Ben Pham <benjamin.pham@gmail.com>
2019-02-26 14:45:24 +0000

    Created User#dashboard, route, basic view and user policy

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :users, only: [:new, :create] do
+    member do
+      get 'dashboard'
+    end
     resources :personalities, only: [:show] do
       resources :booking, only: [:new, :create]
     end
