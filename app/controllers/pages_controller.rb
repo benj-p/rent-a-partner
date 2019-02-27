2991d243c82cc1426bdac8d873a8965259943040
Izzy Weber <izzyweber@gmail.com>
2019-02-27 12:16:40 +0000

    Added filter search to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @personalities = Personality.all
+    @personalities = policy_scope(Personality)
+    @locations_array = [nil]
+    @personalities.each do |personality|
+      @locations_array << personality.user.location
+    end
+    @personalities = policy_scope(Personality.location(params[:location]).gender(params[:gender]))
   end
 end
