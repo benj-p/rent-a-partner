547ab92596eac8feb6920857a8903ca718564435
Ben Pham <benjamin.pham@gmail.com>
2019-02-28 13:48:45 +0000

    Implemenet review / delete display logic depending on date

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,12 +3,12 @@ class Booking < ApplicationRecord
   belongs_to :user
   has_one :review
 
-  validate :booking_date_cannot_be_in_the_past
+  # validate :booking_date_cannot_be_in_the_past
   validates :personality_id, :user_id, presence: true
 
-  def booking_date_cannot_be_in_the_past
-    if date.present? && date < Date.today
-      errors.add(:date, "can't be in the past")
-    end
-  end
+  # def booking_date_cannot_be_in_the_past
+  #   if date.present? && date < Date.today
+  #     errors.add(:date, "can't be in the past")
+  #   end
+  # end
 end
